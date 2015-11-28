99bd83b390959ffc3610b3afc065711c9694ae40
Emmett Agnew <eragnew@gmail.com>
2015-11-27 19:04:11 -0800

    Added login

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var app = express();
+var login = require('./login');
 
-app.set('view engine', 'ejs');
-app.use(express.static('./public'));
+app.set('view engine', 'ejs')
+  .use(express.static('./public'));
+  .use(login.routes);
+  .use(require('./chirps'));
 
-app.get('*', function(req, res) {
-  res.render('index');
+app.get('*', login.required, function(req, res) {
+  res.render('index', {
+    user: login.safe(req.user)
+  });
 });
 
-app.listen(3000);
+app.listen(3000, function() {
+  console.log('server running on 3000...');
+});
