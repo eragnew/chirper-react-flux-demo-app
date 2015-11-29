c94c9ca8b16b86911f14b9d07879ccab562860e5
Emmett Agnew <eragnew@gmail.com>
2015-11-29 11:09:10 -0800

    Added stores constructor

--- src/actions.js
+++ src/actions.js
@@ -1,17 +1,15 @@
 var dispatcher = require('./dispatcher');
 var constants = require('./constants');
 
-exports.chirp = function(data) {
-  dispatcher.dispatch({
-    actionType: constants.CHIRP,
-    data: data
-  });
-};
-
 Object.keys(constants).forEach(function(key) {
   var funcName = key.split('_').map(function(word, i) {
     if (i === 0) return word.toLowerCase();
     return word[0] + word.slice(1).toLowerCase();
-  });
-  console.log(funcName);
+  }).join('');
+  exports[funcName] = function(data) {
+    dispatcher.dispatch({
+      actionType: constants[key],
+      data: data
+    });
+  };
 });
