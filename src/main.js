ad1ff3803559cebcbe32310d68ec53e766a97cd0
Emmett Agnew <eragnew@gmail.com>
2015-11-30 10:42:40 -0800

    Added components

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
+var React = require('react');
+var ReactRouter = require('react-router');
+
+var Route = ReactRouter.Route;
+
 var API = require('./api');
 var ChirpStore = require('./stores/chirps');
 
+var routes = (<Route handler={require('./components/App')}> </Route>);
+
 API.fetchChirps();
+
+ReactRouter.run(routes, ReactRouter.HistoryLocation, function(Root) {
+  React.render(<Root />, document.getElementById('app'));
+});
